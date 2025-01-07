a3537848a25c683a17f2363b523eef2a78090876
mami <kunyataro@gmail.com>
2025-01-08 02:31:46 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,6 +13,7 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
+        console.log('Service Worker: Caching Files');
         return cache.addAll(urlsToCache);
       })
   );
